02ef9a4e9fdee1a7e1c7d56518c5666c545c5c0c
StevieYu <stevieyu@users.noreply.github.com>
2024-06-17 19:01:59 +0800

    Update install.sh

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
 # "$(dirname "$0")/setup-idea-plugins"
+goland_path = "~/.cache/JetBrains/RemoteDev/dist/goland/bin"
+if [ -e "$goland_path" ]; then
+  if ! which "$goland_path" &> /dev/null; then
+    export PATH=$PATH:$goland_path
+  fi
+  goland.sh installPlugins com.intellij.zh
+fi
