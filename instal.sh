9ed928413ad15fa136a6eca0f919a7abe306a6c9
StevieYu <stevieyu@users.noreply.github.com>
2024-06-17 19:11:38 +0800

    Create instal.sh

---
+++ instal.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# "$(dirname "$0")/setup-idea-plugins"
+goland_path="$HOME/.cache/JetBrains/RemoteDev/dist/goland/bin"
+if [ -e "$goland_path" ]; then
+  if ! which "$goland_path" &> /dev/null; then
+    export PATH="$PATH:$goland_path"
+  fi
+  goland.sh installPlugins com.intellij.zh
+  echo "存在goland" > "$HOME/a.txt"
+else
+  echo "不存在goland" > "$HOME/a.txt"
+fi
