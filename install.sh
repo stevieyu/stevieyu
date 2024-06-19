a608c6c5372282fb9d9a0cb903ec68bd3802db44
StevieYu <stevieyu@users.noreply.github.com>
2024-06-19 20:07:14 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,8 +8,7 @@ if [ -e "$goland_path" ]; then
   if ! which "$goland_path" &> /dev/null; then
     export PATH="$PATH:$goland_path"
   fi
-  goland.sh installPlugins com.intellij.zh
-  echo "存在goland" > "$HOME/a.txt"
+  goland.sh installPlugins com.intellij.zh > "$HOME/a.txt"
 else
   echo "不存在goland" > "$HOME/a.txt"
 fi
